daa280d7848fd04b034b329b5748e4714541374e
Conner Pierringer <piercj07@uwgb.edu>
2025-12-06 01:07:04 -0600

    removed unessesary file

--- Advanced Object-Oriented Design Final Project/Header/Manager.h
+++ Advanced Object-Oriented Design Final Project/Header/Manager.h
@@ -4,15 +4,20 @@
 
 class Manager : public Person {
     private:
-        string email;
-        string phonenumber;
+        string userName;
+        string password;
 
     public:
-        Manager(string name, string email, string phonenumber);
-        string getEmail() const;
-        string getPhonenumber() const;
+        Manager(string name, string userName, string password);
+        string getUserName() const;
+        string getPassword() const;
         void print() const override;
         void readAccountsFromFile(const string& filename);
+        void printUserInfo(string customer) const;
+        void printUserName(string customer) const;
+        void printUserAccountType(string customer) const;
+        void printUserBalance(string customer) const;
+        
 
 };
 #endif
